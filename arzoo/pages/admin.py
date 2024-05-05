8934b8fef1509ef94cc981768f8285403ef8ceca
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-05-05 05:37:01 +0300

    added the final touch and further commits will have the deployemnet chores

--- arzoo/pages/admin.py
+++ arzoo/pages/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class cartAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Cart, cartAdmin)
--- arzoo/pages/models.py
+++ arzoo/pages/models.py
@@ -2,9 +2,3 @@ from django.db import models
 
 
 
-class Cart(models.Model):
-    name = models.CharField(max_length=100)  # Example field for product name
-    price = models.IntegerField()  # Example field for product price
-
-    def __str__(self):
-        return self.name
