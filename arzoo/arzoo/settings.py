e2496e9c5d7d3e4854fc15f4dc3675bf3723d3bf
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-05-07 08:04:21 +0300

    a

--- arzoo/arzoo/settings.py
+++ arzoo/arzoo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ot-_cmqnt_zp805lp4ghz+fe$u)%nz2%@uq79mjyh$xbotx802
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://mindslinger.pythonanywhere.com','http://127.0.0.1:8000/','localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['mindslinger.pythonanywhere.com','http://127.0.0.1:8000/','localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
